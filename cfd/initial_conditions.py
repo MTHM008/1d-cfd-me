f8b4b16f80ce97703e2da1e52c04952cec2679d1
Phuong Thao <Phuongthao0812@Phuongs-MacBook-Air.local>
2022-11-24 12:35:05 +0000

    after checking w flake8

--- cfd/initial_conditions.py
+++ cfd/initial_conditions.py
@@ -2,16 +2,19 @@ import numpy as np
 import sympy
 from sympy.utilities.lambdify import lambdify
 
+
 def hat(nx):
     # returns an array of length nx containing 1s for i < nx/3 and i > 2nx/3
     # and 2s for nx/3 <= i <= 2nx/3
-    u = np.ones( nx )
-    u[int(nx/3.): 2*int( nx/3. )] = 2.
+    u = np.ones(nx)
+    u[int(nx/3.): 2*int(nx/3.)] = 2.
     return u
+
 
 def gaussian(x, a, b, c):
     # returns an array with ith value a exp(-(x-b)**2/(2*c**2))
     return np.asarray([a * np.exp(-(xi - b)**2 / (2 * c**2)) for xi in x])
+
 
 def sawtooth(ti, xvals, nuval):
     # returns an array containing a sawtooth function
--- tests/test_advection.py
+++ tests/test_advection.py
@@ -2,14 +2,15 @@ import numpy as np
 from cfd.operators import advection
 from cfd.initial_conditions import gaussian
 
-def test_advect_gaussian():
+
+def test_advect_gaussian():  # INITAL CONDITION FILE - GAUSSION FUNCTION
     nx = 31  # number of grid points
     lx = 2  # length of domains
     dx = lx / (nx - 1)  # grid spacing
     x = np.linspace(0, lx, nx)  # x values
 
     nt = 10  # number of timesteps
-    dt = 0.05  #  timestep
+    dt = 0.05  # timestep
     c = 1  # advection speed
 
     u = gaussian(x, 1, lx/2, 0.2)  # Gaussian initial condition
@@ -19,4 +20,4 @@ def test_advect_gaussian():
         un = u.copy()
         u = un - dt * advection(c, un, dx)
 
-    assert (np.linalg.norm(u-soln) < tol)
+    assert (np.linalg.norm(u-un) < u)
